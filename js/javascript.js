fb06f8dd08c125120675a79206760a6c7eee9322
Jenna Deng <y55deng@pas1098-imac06.uwaterloo.ca>
2018-06-16 18:19:56 -0400

    v1 without menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,1 @@
-$(document).ready(function(){
-            $('.aniview').AniView();
-        });
-        
-$(window).scroll(function() {
-var scroll = $(window).scrollTop();
- //console.log(scroll);
-if (scroll >= 800) {
-    //console.log('a');
-    $("#mainNav").addClass("black-background");
-} else {
-    //console.log('a');
-    $("#mainNav").removeClass("black-background");
-}
-});
+
